ec8a75e4b5fda5f06bfc36dd9b07a58d76452d63
Eduard Abrakov <abrakovem@mail.ru>
2019-10-14 19:53:48 +0500

    create Main

--- src/Main.java
+++ src/Main.java
@@ -11,27 +11,30 @@ public class Main {
     public static void main(String[] args) {
 
         Bank bank = new Bank();
-        //Создаем 100 аккаунтов и записываем в HashMap
         for (int i = 0; i < 100 ; i++) {
+            //Создаем 100 аккаунтов и записываем в список аккаунтов банка
             bank.addAccount("account" + i, new Account("account" + i) );
-            keys.add("account" + i);
+            keys.add("account" + i); //заполняем список названий аккаунтов
+            //пополняем счет случайных акаунтов случайной суммой
             bank.replehnishment(getRandomKey(keys), getRandomCash());
         }
-        //Вызываем 50 транзакций
-        for (int i = 0; i <50 ; i++) {
+
+        for (int i = 0; i <100 ; i++) {
+            //Вызываем 100 транзакций со случайной суммой перевода
             bank.transfer(getRandomKey(keys),getRandomKey(keys), getRandomCash());
         }
 
 
     }
-
+    //Метод получания случайного аккаунта
     public static String getRandomKey ( List list) {
         Random random = new Random();
         return (String) list.get(random.nextInt(list.size()));
     }
 
+    //Метод получения случайного числа типа Long от 0 до 150_000
     public static Long getRandomCash () {
-        long range = 123456L;
+        long range = 150_000L;
         Random r = new Random();
         return (long)(r.nextDouble()*range);
     }
